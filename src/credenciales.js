21df60cdda470340a05f5f0cc51bcccbd1545f14
Josemaria Martinez <207352304+Josemaria-ux@users.noreply.github.com>
2026-04-16 20:33:37 -0300

    Update credenciales.js

--- src/credenciales.js
+++ src/credenciales.js
@@ -1,2 +1,8 @@
 const awsKey = "AKIAIOSFODNN7EXAMPLE";
 const awsSecret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+try {
+    doSomething();
+} catch (error) {
+    // Un bloque catch vacío es un "Major Code Smell".
+}
